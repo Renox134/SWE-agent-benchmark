f9abc2f202d3eaa8f3a12e2fc8711b48ef5fef27
Luca Biermeier <biermeierluca@gmail.com>
2025-11-20 17:35:53 +0100

    prep patches for bench

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import subprocess
+import json
 import sys
+import os
 from datasets import load_dataset
 from pathlib import Path
 
@@ -11,9 +13,12 @@ def main() -> None:
     dataset = load_dataset(dataset_url, split="test")
 
     tasks_base = Path(f"tasks/{agent_model.replace('/', '_')}")
-    tasks_base.mkdir(exist_ok=True)
+    tasks_base.mkdir(parents=True, exist_ok=True)
+
+    patches = []
 
     for idx, task in enumerate(dataset):
+        instance_id = task["instance_id"]
         repo = f"https://github.com/{task['repo']}"
         problem_text = task["problem_statement"]
 
@@ -31,15 +36,36 @@ def main() -> None:
  
         subprocess.run(cmd)
 
-        cmd = [
-            sys.executable, "-m", "swebench.harness.run_evaluation",
-            f"--dataset_name={dataset_url}",
-            f"--predictions_path={str(task_folder)}",
-            "--max_workers=1",
-            f"--run_id=task{idx}"
-        ]
+        folders = os.listdir(str(task_folder))
+        if len(folders) > 0:
+            skip = os.listdir(str(task_folder))[0]
+            patch_path = str(task_folder) + f"/{skip}/{skip}.patch"
 
-        subprocess.run(cmd)
+            if os.path.exists(patch_path):
+                with open(patch_path, "r") as f:
+                    patch_content = f.read()
+
+                    patch_dict = {
+                        "instance_id": instance_id,
+                        "model_name_or_path": agent_model,
+                        "model_patch": patch_content
+                    }
+
+                    patches.append(patch_dict)
+
+    with open(str(tasks_base) + f"/predictions.jsonl", "w") as o:
+        for patch in patches:
+            o.write(json.dumps(patch) + "\n")
+
+    cmd = [
+        sys.executable, "-m", "swebench.harness.run_evaluation",
+        f"--dataset_name={dataset_url}",
+        f"--predictions_path={str(tasks_base) + f"/predictions.jsonl"}",
+        "--max_workers=1",
+        f"--run_id=task{idx}"
+    ]
+
+    subprocess.run(cmd)
 
 if __name__ == "__main__":
     main()
